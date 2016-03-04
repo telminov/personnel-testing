f2add914c002cc6824316d1bc575e04d1d0538af
a.zhukov <sleeeem@mail.ru>
2016-03-04 10:09:03 +0300

    fixed login page

--- auth2/forms.py
+++ auth2/forms.py
@@ -6,7 +6,7 @@ class LoginForm(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs = {'placeholder': 'Логин', 'class': 'form-control'}
+        self.fields['username'].widget.attrs = {'placeholder': 'Email', 'class': 'form-control'}
         self.fields['password'].widget.attrs = {'placeholder': 'Пароль', 'class': 'form-control'}
         self.fields['username'].label = ''
         self.fields['password'].label = ''
